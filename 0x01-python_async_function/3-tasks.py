db19a7ce588db687ce1523f59d28376e9949d39e
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-10 12:36:28 +0000

    new

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,6 +7,7 @@ import asyncio
 from typing import List
 from random import uniform
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     '''
     Return an asyncio.
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,6 +7,7 @@ import asyncio
 from typing import List
 from random import uniform
 
+
 async def task_wait_random(max_delay: int) -> float:
     '''
     Args:
