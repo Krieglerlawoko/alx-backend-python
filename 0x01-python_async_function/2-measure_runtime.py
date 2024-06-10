9ce718f4ffae6b5c7d2a9232968bf77eeb8f4650
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-10 12:53:17 +0000

    change

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ Module to measure runtime of wait_n.
 
 import asyncio
 import time
-from 1-concurrent_coroutines import wait_n
+from concurrent_coroutines import wait_n
 
 def measure_time(n, max_delay):
     '''
