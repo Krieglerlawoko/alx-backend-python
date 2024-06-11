3f9fb6993fddec20f9518c220e59e71fbf7d10cb
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-11 11:19:09 +0000

    he

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Coroutine that collects random numbers using an async comprehension."""
 
-from 0_async_generator import async_generator
+from 0-async_generator import async_generator
+
 
 async def async_comprehension():
     """Collect 10 random numbers from async_generator and return them."""
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,18 +2,14 @@
 """Coroutine to measure runtime of async comprehensions."""
 
 import asyncio
-from time import time
-from 1_async_comprehension import async_comprehension
+from time import perf_counter
+from 1-async_comprehension import async_comprehension
 
 
 async def measure_runtime():
-    """Measure the total runtime of running async_comprehension four times in parallel."""
-    start_time = time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end_time = time()
+    """Measure  total runtime of running
+    async_comprehension four times in parallel."""
+    start_time = perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in 4))
+    end_time = perf_counter()
     return end_time - start_time
